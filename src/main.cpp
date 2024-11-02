418ee49c2dd8623467a02f3a75e04280b941a06f
Zephianah Serotonin <29110377+Zephia-Sero@users.noreply.github.com>
2024-11-02 02:51:11 -0500

    finalized demo

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,11 @@
 #include "atmospherics_device.hpp"
 
 
-void draw_atmosphere(Atmosphere const &atmosphere, double maxTemp, double maxPressure, int x, int y)
+void draw_atmosphere(Atmosphere const &atmosphere, int x, int y)
 {
 	double temp = atmosphere.get_temperature();
 	double pressure = atmosphere.get_pressure();
-	Color color = {
-		static_cast<unsigned char>(255 * temp / maxTemp),
-		0,
-		static_cast<unsigned char>(255 * pressure / maxPressure),
-		255
-	};
-	color = GREEN;
+	Color color = GREEN;
 	double size = 10 * std::cbrt(atmosphere.volume); // dm
 	DrawRectangle(x, y, size, size, color);
 	DrawText(TextFormat("%lfkPa\n\n%lfK", pressure, temp), x + 4, y + 4, 12, BLACK);
@@ -41,20 +35,21 @@ int main()
 {
 	register_atmospherics_builtins();
 
+	atmosphericsElements.add("custom-gas",
+		AtmosphericsElement(
+			"Custom Gas", "CG",
+			19.44831, // heat capacity (J/kgK)
+			88.14490 / 1000, // molar mass (kg/mol)
+			0 // thermal conductivity (W / (m · K))
+		));
+
 	AtmosphericsReaction hydrogenCombustion(CELSIUS(550), 241920);
 	hydrogenCombustion.add_reactant("hydrogen", 2); // 2H2 + O2 = 2H2O
 	hydrogenCombustion.add_reactant("oxygen", 1);
 	hydrogenCombustion.add_product("water", 2);
+	hydrogenCombustion.add_product("custom-gas", 0.01); // woah!
 	hydrogenCombustion.reactionSpeed = 1.0;
 	atmosphericsReactions.push_back(hydrogenCombustion);
-
-	AtmosphericsReaction hydrogenDisplacement(CELSIUS(800), 711204);
-	hydrogenDisplacement.add_reactant("hydrogen", 4); // 4H2 + 2CO2 = C2 + 4H2O
-	hydrogenDisplacement.add_reactant("carbon-dioxide", 2);
-	hydrogenDisplacement.add_product("nitrogen", 1);
-	hydrogenDisplacement.add_product("water", 4);
-	hydrogenDisplacement.reactionSpeed = 1.0;
-	atmosphericsReactions.push_back(hydrogenDisplacement);
 
 	Atmosphere hydrogenTank(ROOM, 1000);
 	hydrogenTank.add_moles_temp("hydrogen", 80, CELSIUS(20));
@@ -62,8 +57,6 @@ int main()
 	oxygenTank.add_moles_temp("oxygen", 80, CELSIUS(20));
 	Atmosphere reactionChamber(ROOM, 1000);
 	Atmosphere waterTank(ROOM, 1000);
-	double maxPressure = 6000;
-	double maxTemp = 34000;
 
 	InitWindow(640, 640, "zatmos");
 	//SetTargetFPS(60);
@@ -79,10 +72,10 @@ int main()
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(WHITE);
-		draw_atmosphere(hydrogenTank, maxTemp, maxPressure, 64, 64);
-		draw_atmosphere(oxygenTank, maxTemp, maxPressure, 64, 256);
-		draw_atmosphere(reactionChamber, maxTemp, maxPressure, 320, (64+256)/2);
-		draw_atmosphere(waterTank, maxTemp, maxPressure, 320, (64+256) / 2 + 256 + 96);
+		draw_atmosphere(hydrogenTank, 64, 64);
+		draw_atmosphere(oxygenTank, 64, 256);
+		draw_atmosphere(reactionChamber, 320, (64+256)/2);
+		draw_atmosphere(waterTank, 320, (64+256) / 2 + 256 + 96);
 		if (mixer.active) {
 			DrawLine(64+100, 64+50, 320, (64+256)/2 + 50, BLACK);
 			DrawLine(64+100, 256+50, 320, (64+256)/2 + 50, BLACK);
@@ -93,7 +86,6 @@ int main()
 			DrawRectangle(320+50 + 16, (64+256) / 2 + 256 + 96, 8, 8, BLUE);
 		EndDrawing();
 		double dt = GetFrameTime();
-		//double t = GetTime();
 		if (IsKeyPressed(KEY_Q))
 			mixer.toggle();
 		if (IsKeyPressed(KEY_F))
@@ -112,6 +104,5 @@ int main()
 		h2oFilter.update(dt);
 		cooler.update(dt);
 		heater.update(dt);
-		//valve.update(dt);
 	}
 }
